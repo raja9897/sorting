a67a8d2d824b545473876dd05ad7cf38598a6446
rajab <rajab@10.12.18.192>
2022-09-19 10:19:40 +0530

    NewSort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,9 +1,33 @@
 package sorting;
-
+//
+//public class BubbleSort {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 int arr[] = {4,7,1,8,9,5,6};
+		 int s = arr.length;
+		 int temp = 0;  
+         for(int i=0; i < s; i++){  
+                 for(int j=1; j < (s-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }
+         for(int i = 0;i<s;i++) {
+        	 System.out.println(arr[i]+" ");
+         }
 
 	}
 
